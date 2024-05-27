78d6dbaa6fec8a65c1a0747be004091e7606aa70
Owmacohe <omch@pm.me>
2024-05-27 19:00:13 -0400

    Finished!

---
+++ Assets/Scripts/AtmosphereController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtmosphereController : MonoBehaviour
+{
+    [SerializeField] Sprite[] backgrounds;
+    [SerializeField] AudioClip[] audios;
+
+    [SerializeField] Image background;
+    [SerializeField] AudioSource audio;
+
+    int current;
+
+    public void Next()
+    {
+        background.sprite = backgrounds[current];
+        
+        audio.Stop();
+        audio.clip = audios[current];
+        audio.Play();
+
+        current++;
+    }
+
+    public void Quit()
+    {
+        Application.Quit(0);
+    }
+}
